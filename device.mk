10576e4a0f26a879b9ea871ca9bf20711e423a5d
boedhack99 <boedhack@gmail.com>
2022-05-19 11:26:16 +0700

    fog: Set TW_OVERRIDE_SYSTEM_PROPS flags

--- device.mk
+++ device.mk
@@ -124,6 +124,8 @@ TARGET_USES_LOGD := true
 TARGET_USES_MKE2FS := true
 TW_NO_SCREEN_BLANK := true
 TW_EXCLUDE_APEX := true
+TW_OVERRIDE_SYSTEM_PROPS := \
+    "ro.build.date.utc;ro.bootimage.build.date.utc=ro.build.date.utc;ro.odm.build.date.utc=ro.build.date.utc;ro.product.build.date.utc=ro.build.date.utc;ro.system.build.date.utc=ro.build.date.utc;ro.system_ext.build.date.utc=ro.build.date.utc;ro.vendor.build.date.utc=ro.build.date.utc;ro.build.product;ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
 
 # Vibrator
 TW_SUPPORT_INPUT_AIDL_HAPTICS := true
